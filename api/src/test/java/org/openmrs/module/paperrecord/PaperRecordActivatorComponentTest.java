8284048c714ae03cefb9da27e30f6f3a0ab7d2ba
djazayeri <jazayeri@alum.mit.edu>
2013-04-07 23:37:09 -0700

    RA-36 - Refactor PIH's EMR, splitting it into API and multiple apps

--- api/src/test/java/org/openmrs/module/paperrecord/PaperRecordActivatorComponentTest.java
+++ api/src/test/java/org/openmrs/module/paperrecord/PaperRecordActivatorComponentTest.java
@@ -1,11 +1,13 @@
 package org.openmrs.module.paperrecord;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
 
 /**
  *
  */
+@Ignore("Test doesn't actually do anything yet")
 public class PaperRecordActivatorComponentTest extends BaseModuleContextSensitiveTest {
 
     @Test
