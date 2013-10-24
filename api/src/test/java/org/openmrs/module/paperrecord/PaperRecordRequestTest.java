e300c43b8dca81c2432b6782a1906c17ceb2ea85
mogoodrich <mgoodrich@pih.org>
2013-10-24 12:18:33 -0400

    fixed test so that it tests against local time zone

--- api/src/test/java/org/openmrs/module/paperrecord/PaperRecordRequestTest.java
+++ api/src/test/java/org/openmrs/module/paperrecord/PaperRecordRequestTest.java
@@ -15,7 +15,9 @@
 package org.openmrs.module.paperrecord;
 
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openmrs.Location;
 import org.openmrs.Patient;
@@ -28,6 +30,13 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PaperRecordRequestTest {
+
+    private String timeZoneCode;
+
+    @Before
+    public void setup() {
+        timeZoneCode = DateTimeZone.getDefault().getShortName(new Date().getTime());
+    }
 
     @Test
     public void testUpdateStatusShouldUpdateStatusLastUpdated() throws InterruptedException {
@@ -63,7 +72,8 @@ public class PaperRecordRequestTest {
         request.setDateCreated(new DateTime(2012, 10, 10, 10, 10).toDate());
         request.setDateStatusChanged(new DateTime(2012, 9, 9, 9, 9).toDate());
 
-        assertThat(request.toString(), is("Paper Record Request: [1 ABC Patient#1 1 2 OPEN Person(personId=1) username Wed Oct 10 10:10:00 EDT 2012 Sun Sep 09 09:09:00 EDT 2012]"));
+        assertThat(request.toString(), is("Paper Record Request: [1 ABC Patient#1 1 2 OPEN Person(personId=1) username Wed Oct 10 10:10:00 "
+                + timeZoneCode + " 2012 Sun Sep 09 09:09:00 " + timeZoneCode +" 2012]"));
     }
 
     @Test
